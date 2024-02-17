d6ac4d6d45e081cecc153d36321e1d0987ccb211
santiag0Forer0 <160348135+santiag0Forer0@users.noreply.github.com>
2024-02-17 14:36:28 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(320, 240);
+}
+
+function draw() {
+  background(20 , 75 , 200);
+circle(160, 120, 180);
+  circle(120, 120, 20)
+  circle(180, 120, 20)
+  line(100, 150, 200, 150);
+  arc(150, 100, 20, 100, 0, HALF_PI);
+  }
